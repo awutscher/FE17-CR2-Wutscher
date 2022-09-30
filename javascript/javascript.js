06e9e581433d01d0bdb767a1539f9d4b53795f05
Alexander Wutscher <alexander.wutscher@chello.at>
2022-09-30 16:29:45 +0200

    weirde neue funtcion

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -121,3 +121,39 @@ for(let i=0; i<done.length; i++){
 function removeThisItem(i){
     document.getElementsByClassName("card")[i].remove();
 }
+
+/* Sort Functionality */
+
+var sortbtn = document.getElementById("sort");
+sortbtn.addEventListener("click", sortByImportance());
+/* fired von selbst ?!?!!?}
+/* function sortByImportance(){
+    tasks.sort((a,b)=>a.importance - b.importance);
+    document.getElementById("content").innerHTML = "";
+    for(let task of tasks){
+        document.getElementById("content").innerHTML += `
+        <div class="card text-center shadow">
+                <img src="https://via.placeholder.com/150" class="card-img-top img-thumbnail" alt="taskimage">
+                <div class="card-body">
+                  <h5 class="card-title">${task.taskName}</h5>
+                  <p class="card-text text">${task.description}</p>
+                  <hr>
+                  <div class="text-start">
+                    <p><i class="bi bi-exclamation-square-fill"></i>
+                         Priority Level 
+                         <span class="importance bg-success p-1 text-white rounded">${task.importance}</span>
+                        <button id="importanceincrease" class="importanceincrease btn"><i class="bi bi-arrow-up-square"></i></button>
+                        <button id="importancedecrease" class="btn"><i class="importancedecrease bi bi-arrow-down-square"></i></button>
+                    </p>
+                    <p><button id="deadlinebutton" class="bi bi-calendar3"></button> Deadline <span>${task.deadline}</span></p>
+                  </div>
+                  <hr>
+                  <div class="text-end">
+                    <button class="remove btn bg-danger"><i class="bi bi-trash"> </i>Delete</button>
+                    <button class="done btn bg-success"><i class="bi bi-check-square"> </i>Done</button>
+                  </div>
+                </div>
+              </div>
+        `
+    };
+} */
